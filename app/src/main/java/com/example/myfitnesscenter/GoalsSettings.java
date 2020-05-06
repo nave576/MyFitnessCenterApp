51c1cea077bc0e5b9ac29139cd1f11a70282f284
Nave <navehatoka100@gmail.com>
2020-05-06 16:01:55 +0300

    new

--- app/src/main/java/com/example/myfitnesscenter/GoalsSettings.java
+++ app/src/main/java/com/example/myfitnesscenter/GoalsSettings.java
@@ -11,4 +11,5 @@ public class GoalsSettings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals_settings);
     }
+    public void func(){}
 }
